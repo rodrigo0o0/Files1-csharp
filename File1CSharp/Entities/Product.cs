acff2317578e623f5d8052ea175be2f61e2c5338
Rodrigo Sandeski <sandeski47@gmail.com>
2022-04-01 03:49:56 -0300

    first commit

---
+++ File1CSharp/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File1CSharp.Entities
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Value { get; set; }
+        public int Amount { get; set; }
+    }
+}
